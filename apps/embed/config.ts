76510d252bb68ef0989170ef18ec28c1bcb04289
enyan27 <truongdat2325@gmail.com>
2026-08-11 11:24:50 +0700

    feat: add initial implementation of the widget

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+  DEFAULT_ORG_ID: "org_3Hkmr1WqCOZsMB7i9uhuEu4g1y2",
+  DEFAULT_POSITION: "bottom-right" as const
+};
